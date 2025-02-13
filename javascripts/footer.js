caa890ac7630a56cd20e3e67f0c8d55bd14faaa0
Chun-Hsiang Yu <kihifung@gmail.com>
2025-02-13 17:03:19 +0800

    Extract footer into footer.js and inject with insertAdjacentHTML

---
+++ javascripts/footer.js
@@ -0,0 +1,9 @@
+const footerHtml = `
+    <footer class="footer py-2 bg-light">
+      <div class="container text-center">
+        <small class="text-black-50 fw-light mb-0">六角學院 React 課程專屬文件</small>
+      </div>
+    </footer>
+  `;
+
+document.body.insertAdjacentHTML("beforeend", footerHtml);
